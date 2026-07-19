bf2e9020b62c260c189f2178aabd27ba22f8b751
yadnesh ranshevare <yranshevare2005@gmail.com>
2026-07-19 20:31:13 +0530

    solve response obj

--- src/app/api/ping/route.ts
+++ src/app/api/ping/route.ts
@@ -3,5 +3,6 @@ export async function GET(req: Request) {
         console.log("ping")
         fetch(`${process.env.VERCEL_URL}`);
     }, Number(process.env.RENDER_DOWNTIME) || 1500);  
-    return new Response("pong");
+    return new Response(JSON.stringify({ message: "pong" }), { status: 200 });
 }
+
