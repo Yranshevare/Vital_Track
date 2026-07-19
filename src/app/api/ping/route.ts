2ca249cd432527095da396b712ce25196ffe0328
yadnesh ranshevare <yranshevare2005@gmail.com>
2026-07-19 20:43:37 +0530

    +

--- src/app/api/ping/route.ts
+++ src/app/api/ping/route.ts
@@ -1,6 +1,6 @@
 export async function GET(req: Request) {
+    console.log("ping")
     setTimeout(() => {
-        console.log("ping")
         fetch(`${process.env.VERCEL_URL}`);
     }, Number(process.env.RENDER_DOWNTIME) || 1500);  
     return new Response(JSON.stringify({ message: "pong" }), { status: 200 });
