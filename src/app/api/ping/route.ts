7ab2b0e46b579b66d2e7111aaddb197e9dcd8c34
yadnesh ranshevare <yranshevare2005@gmail.com>
2026-07-19 20:14:39 +0530

    solve a typo

--- src/app/api/ping/route.ts
+++ src/app/api/ping/route.ts
@@ -1,6 +1,6 @@
 export async function GET(req: Request) {
     setTimeout(() => {
-        fetch(`${process.env.VERCEL_URL}/`);
+        fetch(`${process.env.VERCEL_URL}`);
     }, Number(process.env.RENDER_DOWNTIME) || 1500);  
     return new Response("pong");
 }
